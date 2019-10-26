d55356037aedd50f70bf2ed429b6b8516dcf8b3c
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-26 13:12:30 -0400

    created smurfs reducer and action

---
+++ smurfs/src/reducers/smurfs.js
@@ -0,0 +1,51 @@
+import {
+    GET_SMURF_DATA_REQUEST, 
+    GET_SMURF_DATA_SUCCESS, 
+    GET_SMURF_DATA_FAILURE,
+    POST_SMURF_DATA_REQUEST,
+    POST_SMURF_DATA_SUCCESS,
+    POST_SMURF_DATA_FAILURE} from '../actions';
+
+    const initialState = {
+        smurfsAddition: [],
+        error: ''
+    }
+
+    const reducer = (state = initialState, action) =>{
+        console.log('state:', state);
+        console.log('action:', action);
+        
+        switch(action.type){
+            case GET_SMURF_DATA_REQUEST:
+                return{
+                    ...state
+                }
+                case GET_SMURF_DATA_SUCCESS:
+                        return{
+                            ...state,
+                            smurfsCollection: action.payload
+                        }
+            case GET_SMURF_DATA_FAILURE:
+                        return{
+                            ...state
+                        }
+            case POST_SMURF_DATA_REQUEST:
+                    return{
+                        ...state
+                    }
+            case POST_SMURF_DATA_SUCCESS:
+            return{
+                ...state
+            }
+            case POST_SMURF_DATA_FAILURE:
+                    return{
+                        ...state
+                    }
+            
+            default:
+                return state;
+            
+        }
+    }
+
+    export default reducer;
